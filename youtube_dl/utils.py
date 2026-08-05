0b734d335002cbf97b70b714f901bd995137b104
nicolaasjan <nicoenkarin@gmail.com>
2026-08-05 15:36:12 +0200

    Update Chrome major version range in user agent

--- youtube_dl/utils.py
+++ youtube_dl/utils.py
@@ -100,7 +100,7 @@ compiled_regex_type = compat_re_Pattern
 
 def random_user_agent():
     USER_AGENT_TMPL = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{} Safari/537.36'
-    CHROME_MAJOR_VERSION_RANGE = (138, 148)
+    CHROME_MAJOR_VERSION_RANGE = (141, 151)
     return USER_AGENT_TMPL.format('{0}.0.0.0'.format(random.randint(*CHROME_MAJOR_VERSION_RANGE)))
 
 
